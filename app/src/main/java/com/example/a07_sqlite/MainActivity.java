019648373613ac73fc8ef3f3040a13d06ce13354
sergiopavonalvarez <aergiopavonalvarez@gmail.com>
2023-11-15 12:53:03 +0100

    12:52 15/11

--- app/src/main/java/com/example/a07_sqlite/MainActivity.java
+++ app/src/main/java/com/example/a07_sqlite/MainActivity.java
@@ -19,40 +19,38 @@ public class MainActivity extends AppCompatActivity {
     EditText edad;
     EditText nombre;
     EditText estudios;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        a=new AdminSQlite(this,"Politicos",null,1);
-        nombre=findViewById(R.id.nombre);
-        edad=findViewById(R.id.escaños);
-        estudios=findViewById(R.id.presidente);
+        a = new AdminSQlite(this, "Politicos", null, 1);
+        nombre = findViewById(R.id.nombre);
+        edad = findViewById(R.id.escaños);
+        estudios = findViewById(R.id.presidente);
     }
 
 
-
-
-    public void agregar(View vista){
-
-
-        SQLiteDatabase db= a.getWritableDatabase();
-        ContentValues registro=new ContentValues();
-        registro.put("NOMBRE",nombre.getText().toString());
-        registro.put("EDAD",edad.getText().toString());
-        registro.put("ESTUDIOS",estudios.getText().toString());
-        db.insert("Politicos",null,registro);
+    public void agregar(View vista) {
+        SQLiteDatabase db = a.getWritableDatabase();
+        ContentValues registro = new ContentValues();
+        registro.put("NOMBRE", nombre.getText().toString());
+        registro.put("EDAD", edad.getText().toString());
+        registro.put("ESTUDIOS", estudios.getText().toString());
+        db.insert("Politicos", null, registro);
         //db.close();
     }
-    public void borrar(View vista){
-        EditText nombre=findViewById(R.id.nombre);
 
-        SQLiteDatabase db= a.getWritableDatabase();
-        int filasborradas =db.delete("POLITICOS", "NOMBRE = '"+nombre.getText().toString()+"'",null);
-        if(filasborradas==1){
-            Toast.makeText(this,"se ha borrado lider politico"+nombre.getText().toString(),Toast.LENGTH_LONG);
+    public void borrar(View vista) {
+        EditText nombre = findViewById(R.id.nombre);
 
-        }else{
-            Toast.makeText(this,"no se ha borrado lider politico"+nombre.getText().toString(),Toast.LENGTH_LONG);
+        SQLiteDatabase db = a.getWritableDatabase();
+        int filasborradas = db.delete("POLITICOS", "NOMBRE = '" + nombre.getText().toString() + "'", null);
+        if (filasborradas == 1) {
+            Toast.makeText(this, "se ha borrado lider politico" + nombre.getText().toString(), Toast.LENGTH_LONG);
+
+        } else {
+            Toast.makeText(this, "no se ha borrado lider politico" + nombre.getText().toString(), Toast.LENGTH_LONG);
 
         }
         //db.close();
@@ -73,19 +71,19 @@ public class MainActivity extends AppCompatActivity {
         } else
             Toast.makeText(this, "no se ha encontrado el lider politico " + nombre.getText().toString(), Toast.LENGTH_SHORT);
         //bd.close();
-}
+    }
 
-    public void modificar(View vista){
+    public void modificar(View vista) {
         SQLiteDatabase bd = a.getWritableDatabase();
-        ContentValues s=new ContentValues();
-        s.put("edad",edad.getText().toString());
-        s.put("estudios",estudios.getText().toString());
-        int filasupdateadas=bd.update("politicos",s,"nombre='"+nombre.getText().toString()+"'",null);
-        if(filasupdateadas==1){
-            Toast.makeText(this,"se ha modificado lider politico"+nombre.getText().toString(),Toast.LENGTH_LONG);
+        ContentValues s = new ContentValues();
+        s.put("edad", edad.getText().toString());
+        s.put("estudios", estudios.getText().toString());
+        int filasupdateadas = bd.update("politicos", s, "nombre='" + nombre.getText().toString() + "'", null);
+        if (filasupdateadas == 1) {
+            Toast.makeText(this, "se ha modificado lider politico" + nombre.getText().toString(), Toast.LENGTH_LONG);
 
-        }else{
-            Toast.makeText(this,"no se ha modificado lider politico"+nombre.getText().toString(),Toast.LENGTH_LONG);
+        } else {
+            Toast.makeText(this, "no se ha modificado lider politico" + nombre.getText().toString(), Toast.LENGTH_LONG);
 
         }
         //bd.close();
